6d855fc99a39a9fcf9e0de8f06b6c7e041df774b
Bernard Teye <datssosh@gmail.com>
2024-10-23 05:25:59 +0000

    add createAdmin route handler in SeederController file

--- src/seeder/seeder.controller.ts
+++ src/seeder/seeder.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller('seeder')
-export class SeederController {}
+import { SeederService } from './seeder.service';
+import { ApiTags } from '@nestjs/swagger';
+
+@Controller('seeders')
+@ApiTags('Seeder')
+export class SeederController {
+  constructor(private seederService: SeederService) {}
+
+  @Get('create-admin')
+  async createAdmin() {
+    return await this.seederService.createAdmin();
+  }
+}
--- src/seeder/seeder.service.ts
+++ src/seeder/seeder.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { hash } from 'bcryptjs';
+
+import { Role } from '@prisma/client';
+import { UserRepository } from 'src/database/repository/user.repository';
 
 @Injectable()
-export class SeederService {}
+export class SeederService {
+  constructor(
+    private userRepo: UserRepository,
+    private configService: ConfigService,
+  ) {}
+
+  async createAdmin() {
+    const adminDetails = this.configService.get<{
+      name: string;
+      username: string;
+      password: string;
+    }>('admin');
+
+    try {
+      const existingAdmin = await this.userRepo.find(adminDetails.username);
+
+      if (existingAdmin) {
+        throw new Error('Admin already exist');
+      }
+
+      const hashedPassword = await hash(adminDetails.password, 12);
+      await this.userRepo.add({
+        ...adminDetails,
+        password: hashedPassword,
+        role: Role.ADMIN,
+      });
+
+      return { message: 'Admin successfully created' };
+    } catch (error) {
+      throw new BadRequestException(error.message || 'Something went wrong');
+    }
+  }
+}
