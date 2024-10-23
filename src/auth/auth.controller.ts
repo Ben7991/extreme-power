a0f7d0fa7a98fe4c6f444a939f7c6b8027fbf33a
Bernard Teye <datssosh@gmail.com>
2024-10-23 05:36:10 +0000

    add login and refreshToken route handlers to AuthController and it's corresponding functions in AuthService as well as generateAuthToken in AuthService

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,69 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { Request, Response } from 'express';
+
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+@ApiTags('Auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  async login(
+    @Body(ValidationPipe) loginDto: LoginDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const result = await this.authService.login(
+      loginDto.username,
+      loginDto.password,
+    );
+
+    this.setAuthCookie(res, result.token);
+
+    return {
+      message: result.message,
+      data: result.user,
+    };
+  }
+
+  private setAuthCookie(
+    res: Response,
+    token: { refreshToken: string; accessToken: string },
+  ) {
+    const refreshTokenDuration = 1000 * 60 * 60 * 24 * 30;
+
+    res.cookie('_ref-tk', token.refreshToken, {
+      path: '/',
+      httpOnly: true,
+      maxAge: refreshTokenDuration,
+    });
+
+    res.cookie('_acc-tk', token.accessToken, {
+      path: '/',
+      httpOnly: true,
+      maxAge: refreshTokenDuration,
+    });
+  }
+
+  @Get('refresh-token')
+  async refreshToken(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const refreshToken = <string>req.cookies['_ref-tk'];
+    const result = await this.authService.refreshToken(refreshToken);
+    this.setAuthCookie(res, result);
+
+    return { message: 'Refreshed token' };
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,81 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AccountStatus } from '@prisma/client';
+import { compare } from 'bcryptjs';
+import { sign, verify } from 'jsonwebtoken';
+
+import { UserRepository } from 'src/database/repository/user.repository';
+import { User } from 'src/shared/types/user.type';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userRepo: UserRepository,
+    private configService: ConfigService,
+  ) {}
+
+  async login(username: string, pass: string) {
+    const existingUser = await this.userRepo.find(username);
+    const hashedPassword = existingUser ? existingUser.password : '';
+    const samePassword = await compare(pass, hashedPassword);
+
+    if (
+      !existingUser ||
+      !samePassword ||
+      existingUser.status === AccountStatus.SUSPENDED
+    ) {
+      throw new BadRequestException('Invalid login credentails');
+    }
+
+    return {
+      message: 'You are successfully logged-in',
+      token: this.generateAuthToken(existingUser),
+      user: {
+        id: existingUser.id,
+        name: existingUser.name,
+        username: existingUser.username,
+        role: existingUser.role,
+      },
+    };
+  }
+
+  private generateAuthToken(user: User) {
+    const secretKey = this.configService.get<string>('secretKey');
+
+    const refreshToken = sign(
+      { sub: user.id, username: user.username },
+      secretKey,
+      { expiresIn: '30days' },
+    );
+
+    const accessToken = sign({ sub: refreshToken }, secretKey, {
+      expiresIn: '15mins',
+    });
+
+    return { refreshToken, accessToken };
+  }
+
+  async refreshToken(refreshToken: string) {
+    const secretKey = this.configService.get<string>('secretKey');
+
+    try {
+      const result = <{ sub: string; username: string }>(
+        verify(refreshToken, secretKey)
+      );
+
+      const existingUser = await this.userRepo.find(result.username);
+
+      if (!existingUser) {
+        throw new Error();
+      }
+
+      return this.generateAuthToken(existingUser);
+    } catch (error) {
+      throw new UnauthorizedException('Invalid token');
+    }
+  }
+}
