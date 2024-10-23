55f42827ef5781319a17198560248859edc55068
Bernard Teye <datssosh@gmail.com>
2024-10-23 05:24:41 +0000

    add admin credentals to environment variables

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,11 @@
 const envConfig = () => ({
   port: process.env.PORT,
   secretKey: process.env.SECRET_KEY,
+  admin: {
+    name: process.env.ADMIN_NAME,
+    username: process.env.ADMIN_USERNAME,
+    password: process.env.ADMIN_PASSWORD,
+  },
 });
 
 export { envConfig };
